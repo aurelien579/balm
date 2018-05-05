38c0e7856dd124e5b041d2141c321a271d86b940
Martin Aurelien <amartin3@TC405-009-12.insa-lyon.fr>
2018-05-05 16:48:29 +0200

    Login page corrected

--- routes/user.js
+++ routes/user.js
@@ -2,47 +2,62 @@ var express = require('express');
 var router = express.Router();
 var userModel = require('../models/user');
 
+router.post('/login', function(req, res, next) {
+    userModel.getByUsername(req.body.username, (err, user) => {
+        req.session.user = user;
+
+        if (user !== undefined) {
+            var successMessage = "Vous êtes bien connecté";
+        } else {
+            var errorMessage = "Erreur lors de la connection";
+        }
+
+        res.render('login', {
+            title: 'Connexion',
+            user: req.session.user,
+            successMessage: successMessage,
+            errorMessage: errorMessage
+        });
+    });
+});
+
 router.get('/login', function(req, res, next) {
-  res.render('login', { user: req.session.user, title: 'Connexion' });
+    res.render('login', {
+        user: req.session.user,
+        title: 'Connexion'
+    });
 });
 
 router.post('/register', function(req, res, next) {
-  userModel.create(req.body.username, req.body.password, req.body.email, (err, user) => {
-    if (err) {
-      var errorMessage = err.message;
-      if (err.code == userModel.USERNAME_EXISTS) {
-        errorMessage = "Erreur lors de la création du compte, ce nom d'utilisateur est déjà pris";
-      } else if (err.code == userModel.EMAIL_EXISTS) {
-        errorMessage = "Erreur lors de la création du compte, cet email est déjà prise";
-      }
+    userModel.create(req.body.username, req.body.password, req.body.email, (err, user) => {
+        if (err) {
+            var errorMessage = err.message;
+            if (err.code == userModel.USERNAME_EXISTS) {
+                errorMessage = "Erreur lors de la création du compte, ce nom d'utilisateur est déjà pris";
+            } else if (err.code == userModel.EMAIL_EXISTS) {
+                errorMessage = "Erreur lors de la création du compte, cet email est déjà prise";
+            }
 
-      res.render('login', { title: 'Erreur', user: req.session.user,
-                            errorMessage: errorMessage });
-    } else {
-      res.render('login', { title: 'Compte créé', user: req.session.user,
-                            successMessage: 'Votre compte a bien été créé' });
-    }
-  });
-});
-
-router.post('/login', function(req, res, next) {
-  userModel.getByUsername(req.body.username, (err, user) => {
-    req.session.user = user;
-
-    if (user !== undefined) {
-      var successMessage = "Vous êtes bien connecté";
-    } else {
-      var errorMessage = "Erreur lors de la connection";
-    }
-
-    res.render('login', { title: 'Connexion', user: req.session.user, successMessage: successMessage,
-                          errorMessage: errorMessage });
-  });
+            res.render('login', {
+                title: 'Erreur',
+                user: req.session.user,
+                errorMessage: errorMessage
+            });
+        } else {
+            res.render('login', {
+                title: 'Compte créé',
+                user: req.session.user,
+                successMessage: 'Votre compte a bien été créé'
+            });
+        }
+    });
 });
 
 router.get('/logout', function(req, res, next) {
-  req.session.user = undefined;
-  res.render('index', { title: 'BALM' });
+    req.session.user = undefined;
+    res.render('index', {
+        title: 'BALM'
+    });
 });
 
 module.exports = router;
