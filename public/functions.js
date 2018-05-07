30c2a5607d3f5d63f4527e7e92d7ff8505cd99a5
Sabathier Alexandre <sabathieralexandre@yahoo.fr>
2018-05-07 18:58:50 +0200

    ha oai oai oai

--- public/functions.js
+++ public/functions.js
@@ -12,10 +12,14 @@ var yyyy = today.getFullYear();
 today = yyyy+'-'+mm+'-'+dd;
 document.getElementById("date").setAttribute("min", today);
 
-/*function fixclick() {
-  var x = document.getElementById("barre");
-  x.style.display = "none";
-}
-$(function() {
-  fixclick();
-});*/
+$(document).ready(function(){
+  var date_input=$('input[name="date"]'); //our date input has the name "date"
+  var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+  var options={
+    format: 'mm/dd/yyyy',
+    container: container,
+    todayHighlight: true,
+    autoclose: true,
+  };
+  date_input.datepicker(options);
+})
