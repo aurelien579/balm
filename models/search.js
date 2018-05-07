76f244138e35f78b3aa1fff24283f4544e61d088
mael <mael.brouard@hotmail.fr>
2018-05-07 12:58:42 +0200

    Search11

--- models/search.js
+++ models/search.js
@@ -2,8 +2,8 @@ const pool = require('./db').pool;
 
 let offerCityGetSql = 'SELECT * FROM Offer WHERE city = ?';
 
-var getByOfferCity = function(city, callback) {
-    pool.query(offerCityGetSql, [city], (err, results) => {
+var getByOfferCity = function(search, callback) {
+    pool.query(offerCityGetSql, [search], (err, results) => {
         if (err) {
             callback(err);
         } else {
--- routes/search.js
+++ routes/search.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var searchModel = require('../models/search');
 
 router.get('/', function(req, res, next) {
+  var search = req.query.searchText;  // Récupère le texte de recherche
+  var search_class = "city";
   searchModel.getByOfferCity(req.query.searchText, (err, results) => {
-    var search = req.query.searchText;  // Récupère le texte de recherche
+    var house = [];
     var house_lgt = results.length;
     // Passe d'un tableau 1D à tableau 2D
-    var house = [];
     for(var i = 0; i < results.length; i += 3) {
       house[i/3] = [];
       for (var j = 0; j < 3; j++) {
         if ((i+j) < results.length) {
-          //house[i/3][j] = [];
           house[i/3][j] = results[i+j];
         }
       }
