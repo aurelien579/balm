1ef4e8237afec5c4e4cd8ea407cfea34c4209d07
Aurélien Martin <aurlienmartin@gmail.com>
2018-05-07 12:16:51 +0200

    Added getByUserId in goods.js model

--- models/goods.js
+++ models/goods.js
@@ -1,16 +1,24 @@
 const pool = require('./db').pool;
 
 
-let sqlGetById = 'SELECT * FROM Offer WHERE Id = ?';
+let sqlGetById = 'SELECT * FROM Offer WHERE id = ?';
+let sqlGetByUserId = 'SELECT * FROM Offer WHERE userId = ?;'
 
-var getById = function (id, callback) {
-    pool.query(sqlGetById,[id], (err, results) => {
-        if(err){
+var getById = function(id, callback) {
+    pool.query(sqlGetById, [id], (err, results) => {
+        if (err) {
             callback(err);
-        }else{
-            callback(err,results[0]);
+        } else {
+            callback(err, results[0]);
         }
     });
 }
 
+var getByUserId = function(userId, callback) {
+    pool.query(sqlGetByUserId, [userId], (err, results) => {
+        callback(err, results);
+    });
+}
+
+exports.getByUserId = getByUserId;
 exports.getById = getById;
