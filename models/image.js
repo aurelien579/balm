b1d89830b9ff4b71efa98c32809f4eb959983b67
Aurélien Martin <aurlienmartin@gmail.com>
2018-05-08 10:05:24 +0200

    Corrected image sql request

--- models/image.js
+++ models/image.js
@@ -1,6 +1,6 @@
 const pool = require('./db').pool;
 
-let sqlGetByUserId = "SELECT path, offerId FROM Image, Offer, User WHERE User.id = Offer.userId AND Image.offerId = Offer.id WHERE User.id = ?;";
+let sqlGetByUserId = "SELECT path, offerId FROM Image, Offer, User WHERE User.id = Offer.userId AND Image.offerId = Offer.id AND User.id = ?;";
 
 var getByUserId = function(userId, callback) {
     pool.query(sqlGetByUserId, userId, callback);
