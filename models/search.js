1258cc1cc7734fa320ce82b69a9c5fef7156054b
mael <mael.brouard@hotmail.fr>
2018-05-08 11:37:36 +0200

    Search11

--- models/search.js
+++ models/search.js
@@ -1,15 +1,48 @@
 const pool = require('./db').pool;
 
-let offerCityGetSql = 'SELECT * FROM Offer WHERE city = ?';
+let findRegionGetSql = 'SELECT COUNT(*) FROM Region WHERE regionname = ?;';
+let findDepartmentGetSql = 'SELECT COUNT(*) FROM Department WHERE departmentname = ?;';
+let findCityGetSql = 'SELECT COUNT(*) FROM City WHERE cityname = ?;';
+let findPostCodeGetSql = 'SELECT COUNT(*) FROM City WHERE postcode = ?;';
 
-var getByOfferCity = function(search, callback) {
-    pool.query(offerCityGetSql, [search], (err, results) => {
-        if (err) {
-            callback(err);
-        } else {
-            callback(err, results);
-        }
-    });
+let offerRegionGetSql = 'SELECT * FROM Offer WHERE region = ?;';
+let offerDepartmentGetSql = 'SELECT * FROM Offer WHERE department = ?;';
+let offerCityGetSql = 'SELECT * FROM Offer WHERE city = ?;';
+let offerPostCodeGetSql = 'SELECT * FROM Offer WHERE postcode = ?;';
+
+var getByFindRegion = function(search, callback) {
+  pool.query(findRegionGetSql, [search], callback);
 }
 
-exports.getByOfferCity = getByOfferCity;
+var getByFindDepartment = function(search, callback) {
+  pool.query(findDepartmentGetSql, [search], callback);
+}
+
+var getByFindCity = function(search, callback) {
+  pool.query(findCityGetSql, [search], callback);
+}
+
+var getByFindPostCode = function(search, callback) {
+  pool.query(findPostCodeGetSql, [search], callback);
+}
+
+var getByOffer = function(search, search_class, callback) {
+  if (search_class == 0) {
+    callback(err);
+  } else if (search_class == 1) {
+    pool.query(offerRegionGetSql, [search], callback);
+  } else if (search_class == 2) {
+    pool.query(offerDepartmentGetSql, [search], callback);
+  } else if (search_class == 3) {
+    pool.query(offerCityGetSql, [search], callback);
+  } else if (search_class == 4) {
+    pool.query(offerPostCodeGetSql, [search], callback);
+  }
+}
+
+exports.getByFindRegion = getByFindRegion;
+exports.getByFindDepartment = getByFindDepartment;
+exports.getByFindCity = getByFindCity;
+exports.getByFindPostCode = getByFindPostCode;
+
+exports.getByOffer = getByOffer;
--- routes/search.js
+++ routes/search.js
@@ -3,17 +3,41 @@ var router = express.Router();
 var searchModel = require('../models/search');
 
 router.get('/', function(req, res, next) {
-  var search = req.query.searchText;  // Récupère le texte de recherche
-  var search_class = "city";
-  searchModel.getByOfferCity(req.query.searchText, (err, results) => {
+  var search = req.query.searchText; // Récupère le texte de recherche
+  var search_class;
+
+  searchModel.getByFindRegion(search, (err, result) => {
+    if (result == 0) {
+      searchModel.getByFindDepartment(search, (err, result) => {
+        if (result == 0) {
+          searchModel.getByFindCity(search, (err, result) => {
+            if (result == 0) {
+              searchModel.getByFindPostCode(search, (err, result) => {
+                if (result == 0)
+                  search_class = 0;
+                else search_class = 4;
+              });
+            } else search_class = 3;
+          });
+        } else search_class = 2;
+      });
+    } else search_class = 1;
+  })
+
+  searchModel.getByOffer(search, search_class, (err, results) => {
+    if (err) {
+      res.render('error', {
+        error: err
+      });
+    }
     var house = [];
     var house_lgt = results.length;
     // Passe d'un tableau 1D à tableau 2D
-    for(var i = 0; i < results.length; i += 3) {
-      house[i/3] = [];
+    for (var i = 0; i < results.length; i += 3) {
+      house[i / 3] = [];
       for (var j = 0; j < 3; j++) {
-        if ((i+j) < results.length) {
-          house[i/3][j] = results[i+j];
+        if ((i + j) < results.length) {
+          house[i / 3][j] = results[i + j];
         }
       }
     }
