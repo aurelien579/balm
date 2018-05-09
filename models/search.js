13f96e7fde061635528f8fe2d17b62de7ec69103
mael <mael.brouard@hotmail.fr>
2018-05-09 12:06:51 +0200

    Search14

--- models/search.js
+++ models/search.js
@@ -1,24 +1,21 @@
 const db = require('./db');
 
-const offerGetSql = 'SELECT * FROM Offer WHERE postcode = ? OR city = ? OR department = ? OR region = ?;'
-const testofferGetSql ='SELECT o.* FROM Offer AS o, Availability AS a WHERE o.id = a.offerId AND (o.postcode = ? OR o.city = ? OR o.department = ? OR o.region = ?) AND a.start <= ? AND a.end >= ?;'
-
-const offer1 = 'SELECT O.* FROM Offer AS O, Availability AS A WHERE O.id = A.offerId'
-const offer2 = ' AND (O.postcode = ? OR O.city = ? OR O.department = ? OR O.region = ?)'
-const offer3 = ' AND A.start <= ?'
-const offer3 = ' AND A.end >= ?'
-const offerclose = ';'
+const offerGetSqlBase = 'SELECT O.* FROM Offer AS O, Availability AS A WHERE O.id = A.offerId'
+const offerGetSqlText = ' AND (O.postcode = ? OR O.city = ? OR O.department = ? OR O.region = ?)'
+const offerGetSqlDatedep = ' AND A.start <= ?'
+const offerGetSqlDatearr = ' AND A.end >= ?'
+const offerGetSqlClose = ';'
 
 var getByOffer = function(search, callback) {
-  const offertest = offer1;
+  let offerGetSql = offerGetSqlBase;
   if (!!search.text)
-    offertest += offer2;
+    offerGetSql += offerGetSqlText;
   if (!!search.datedep)
-    offertest += offer3;
+    offerGetSql += offerGetSqlDatedep;
   if (!!search.datearr)
-    offertest += offer4;
-  offertest += offerclose;
-  return db.sqlQuery(testofferGetSql, [search.text, search.text, search.text, search.text, search.datedep, search.datearr]);
+    offerGetSql += offerGetSqlDatearr;
+  offerGetSql += offerGetSqlClose;
+  return db.sqlQuery(offerGetSql, [search.text, search.text, search.text, search.text, search.datedep, search.datearr]);
 }
 /*
 var getByOffer = function(search, callback) {
