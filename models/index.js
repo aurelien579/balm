34f665032f82b26e0b190d37e59a808e4efb4d81
Sabathier Alexandre <sabathieralexandre@yahoo.fr>
2018-05-09 12:26:22 +0200

    hfrke

--- models/index.js
+++ models/index.js
@@ -1,9 +1,10 @@
 const pool = require('./db').pool;
+const db = require('./db');
 
 const sqlGetCities = "SELECT * FROM City WHERE ville_nom_reel = '?%';"
 
-function getCitiesName(startW, callback) {
-    pool.query(sqlGetCities, [startW], callback);
+function getCitiesName(startW) {
+    return db.sqlQuery(sqlGetCities, [startW]);
 }
 
 exports.getCitiesName = getCitiesName;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var searchCity = require('../models/index');
 
+
+/*router.post('/', function(req, res, next) {
+  var starW = req.query.
+  searchCity.getCitiesName(startW)
+});*/
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
