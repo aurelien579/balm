fc7177273645446a064d21e8c452784e31011084
mael <mael.brouard@hotmail.fr>
2018-05-09 11:33:43 +0200

    Search12

--- sql/dump.sql
+++ sql/dump.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `Availability`
 --
 
+USE Balm;
+
 DROP TABLE IF EXISTS `Availability`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
