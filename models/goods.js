ee09ecdcf4f917df52924bec6ed3c89ad443ab81
Aurélien Martin <aurlienmartin@gmail.com>
2018-05-09 10:55:12 +0200

    wip

--- models/goods.js
+++ models/goods.js
@@ -3,6 +3,15 @@ const db = require('./db');
 const sqlGetById = 'SELECT * FROM Offer WHERE id = ?;'
 const sqlGetByUserId = 'SELECT * FROM Offer WHERE userId = ?;'
 
+const slqGetByUserIdWithFirstImage =
+    `SELECT Offer.title, Offer.description, Offer.price, Image.path FROM Offer LEFT JOIN Image ON Image.id =
+    (
+    	SELECT id FROM Image
+    	WHERE Image.offerId = Offer.id
+    	ORDER BY id
+    	LIMIT 1
+    ) WHERE Offer.userId = ?;`;
+
 var getById = function(id, callback) {
     db.pool.query(sqlGetById, [id], (err, results) => {
         if (err) {
@@ -17,5 +26,10 @@ function getByUserId(userId) {
     return db.sqlQuery(sqlGetByUserId, [userId]);
 }
 
+function getByUserIdWithFirstImage(userId) {
+    return db.sqlQuery(slqGetByUserIdWithFirstImage, [userId]);
+}
+
 exports.getByUserId = getByUserId;
 exports.getById = getById;
+exports.getByUserIdWithFirstImage = getByUserIdWithFirstImage;
--- routes/user.js
+++ routes/user.js
@@ -91,34 +91,8 @@ router.get('/infos', mustBeConnected, function(req, res, next) {
 });
 
 router.get('/offers', mustBeConnected, function(req, res, next) {
-    goodsModel.getByUserId(req.session.user.id)
+    goodsModel.getByUserIdWithFirstImage(req.session.user.id)
         .then((offers) => {
-            let promises = [];
-
-            offers.forEach((offer) => {
-                promises.push(new Promise((resolve, reject) => {
-                    imageModel.getFirstByOfferId(offer.id)
-                        .then((images) => {
-                            if (images.length > 0)
-                                offer.image = images[0];
-                            else {
-                                offer.image = {
-                                    path: imageModel.DEFAULT_OFFER_IMAGE
-                                }
-                            }
-                            resolve(offer);
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                            reject(err);
-                        });
-                }));
-            });
-
-            return Promise.all(promises);
-        })
-        .then((offers) => {
-            console.log(offers);
             res.render('user/user-offers', {
                 user: req.session.user,
                 offers: offers
@@ -132,7 +106,7 @@ router.get('/offers', mustBeConnected, function(req, res, next) {
 });
 
 router.get('/comments', mustBeConnected, function(req, res, next) {
-    res.render('uesr/comments');
+    res.render('user/user-comments');
 });
 
 module.exports = router;
