3bee63f71d2dbdcce78d380352d45dabac084a7d
Aurélien Martin <aurlienmartin@gmail.com>
2018-05-10 11:52:56 +0200

    reservations

--- models/reservation.js
+++ models/reservation.js
@@ -1,6 +1,30 @@
 const db = require('./db');
 
-const sqlGetByUserId = "SELECT * FROM Reservation, Offer WHERE Reservation.offerId == Offer.id AND userId = ?;";
+const sqlGetByUserId = `
+SELECT
+    DATE_FORMAT(Reservation.from, '%d %M %Y') AS 'from',
+    DATE_FORMAT(Reservation.to, '%d %M %Y') AS 'to',
+    Reservation.status,
+    Reservation.offerId,
+    Offer.title,
+    Offer.price,
+    Image.path
+FROM
+    Reservation,
+    Offer
+        LEFT JOIN
+    Image ON Image.id = (SELECT
+            id
+        FROM
+            Image
+        WHERE
+            Image.offerId = Offer.id
+        ORDER BY id
+        LIMIT 1)
+WHERE
+    Reservation.userId = 1
+        AND Offer.id = Reservation.offerId;
+`;
 
 function getByUserId(userId) {
     return db.sqlQuery(sqlGetByUserId, [userId]);
