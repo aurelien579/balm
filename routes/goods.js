699467a67419bf0122452ec707f6f736efae41e6
Aurélien Martin <aurlienmartin@gmail.com>
2018-05-10 13:06:48 +0200

    new good form

--- routes/goods.js
+++ routes/goods.js
@@ -4,6 +4,7 @@ const goodsModel = require('../models/goods');
 const commentModel = require('../models/comment');
 const imageModel = require('../models/image');
 const app = require('../app');
+const utils = require('./utils');
 
 // req -> recuperer les informations que le client m'envoi
 // res -> reponse a envoyer au client
@@ -18,7 +19,9 @@ router.get('/', function(req, res, next) {
     next();
 });
 
-
+router.get('/new', utils.mustBeConnected, function(req, res, next) {
+    res.render('goods-new');
+});
 
 router.get('/:id', function(req, res, next) {
     //  var sea = req.params.searchText;
@@ -27,26 +30,26 @@ router.get('/:id', function(req, res, next) {
     let comments;
 
     goodsModel.getById(id)
-      .then((_offers) => {
-        offer = _offers[0];
-        return commentModel.getByOfferId(id);
-      })
-      .then((_comments) => {
-        comments = _comments;
-        return imageModel.getByOfferId(id);
-      })
-      .then((images) => {
-        res.render('goods', {
-            offer: offer,
-            comments: comments,
-            images: images
+        .then((_offers) => {
+            offer = _offers[0];
+            return commentModel.getByOfferId(id);
+        })
+        .then((_comments) => {
+            comments = _comments;
+            return imageModel.getByOfferId(id);
+        })
+        .then((images) => {
+            res.render('goods', {
+                offer: offer,
+                comments: comments,
+                images: images
+            });
+        })
+        .catch((err) => {
+            res.render('error', {
+                error: err
+            });
         });
-      })
-      .catch((err) => {
-          res.render('error', {
-              error: err
-          });
-      });
 
 });
 
