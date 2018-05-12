6af3609062504398a6cd0fef993071dfae0e519f
Camille <camimot@hotmail.fr>
2018-05-12 12:54:50 +0200

    redirection login

--- models/availability.js
+++ models/availability.js
@@ -6,4 +6,12 @@ function add(offerId, from, to) {
     return db.sqlQuery(sqlAdd, [offerId, from, to]);
 }
 
+
+const sqlGetAvailabilityByOfferId = "SELECT * FROM Availability WHERE offerId = ?;";
+
+function getAvailabilityByOfferId(offerId) {
+    return db.sqlQuery(sqlGetAvailabilityByOfferId, [offerId]);
+}
+
 exports.add = add;
+exports.getAvailabilityByOfferId = getAvailabilityByOfferId;
--- routes/reservation.js
+++ routes/reservation.js
@@ -9,11 +9,13 @@ const reservationModel = require('../models/reservation');
 const app = require('../app');
 const utils = require('./utils');
 
-router.get('/new', utils.mustBeConnected, function(req, res, next) {
+router.get('/new', function(req, res, next) {
     let from = req.query.from; // Récupère le texte de recherche
     let to = req.query.to;
     let numberpers = req.query.numberpers;
     let id = req.query.id;
+    let url: req.url;
+    utils.mustBeConnectedToBook(req, res, next);
 
     reservationModel.createReservation(id, req.session.user.id, from, to, 0)
         .then((result) => {
