be7657ae73d78371f1666c0409714ce8ad28e9ed
Aurélien Martin <aurlienmartin@gmail.com>
2018-05-30 10:51:04 +0200

    fixed about

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const goodsRouter = require('./routes/goods');
 const reservationRouter = require('./routes/reservation');
 const commentRouter = require('./routes/comment');
 const messageRouter = require('./routes/message');
-const aboutRouter = require('./routes/about');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -61,7 +60,6 @@ app.use('/goods', goodsRouter);
 app.use('/reservation', reservationRouter);
 app.use('/comment', commentRouter);
 app.use('/message', messageRouter);
-app.use('/about', aboutRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
